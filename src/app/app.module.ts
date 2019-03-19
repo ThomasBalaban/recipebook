9e0880fd81819782b840aaafba49d2ff9f33dbc0
dksc104868 <thomas.balaban@dcsg.com>
2019-03-19 19:00:08 -0400

    r2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component'
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component'
+import { IngredientComponent } from './recipe/ingredient/ingredient.component'
+import { HeaderComponent } from './header/header.component'
+import { RecipeBookComponent } from './recipe/recipe-book/recipe-book.component'
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component'
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component'
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component'
+import { RecipeComponent } from './recipe/recipe.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+    HeaderComponent,
+    RecipeBookComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
