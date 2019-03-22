7ff0fafff6ad1f8e8f323f20eaa79bea542a40a6
ThomasBalaban <balabaninteractive@gmail.com>
2019-03-21 22:51:26 -0400

    shopping list and recipes done

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model'
 })
 
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
+  recipe: Recipe[] = [
     new Recipe('chicken parm', 'chicken with parm', 'https://www.spendwithpennies.com/wp-content/uploads/2018/11/SpendWithPennies-Chicken-Parmesan-24.jpg'),
     new Recipe('chicken parm2', 'chicken with parm', 'https://www.spendwithpennies.com/wp-content/uploads/2018/11/SpendWithPennies-Chicken-Parmesan-24.jpg')
   ]
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core'
+import { Ingredient } from 'src/app/shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
   styleUrls: ['./shopping-list-edit.component.css']
 })
+
 export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef
+  @ViewChild('amountInput') amountInputRef: ElementRef
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value
+    const ingAmount = this.amountInputRef.nativeElement.value
+
+    const newIngredient = new Ingredient(ingName, ingAmount)
+    this.ingredientAdded.emit(newIngredient)
   }
 
 }
