2680fbafea2ef108f88d5fe18438910d7d940a7a
dksc104868 <thomas.balaban@dcsg.com>
2019-04-11 13:59:06 -0400

    auth stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    const config = {
+      apiKey: 'AIzaSyBeHwsjv7uFJRpgZWYoN4dhnJ4ntu_fFBY',
+      authDomain: 'recipes-f17a8.firebaseapp.com',
+      databaseURL: 'https://recipes-f17a8.firebaseio.com',
+      projectId: 'recipes-f17a8',
+      storageBucket: 'recipes-f17a8.appspot.com',
+      messagingSenderId: '933068040336'
+    };
+    firebase.initializeApp(config);
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { Response } from '@angular/http';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements DoCheck {
+  init: boolean;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+  ngDoCheck() {
+    this.init = this.authService.isAuthenticated();
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
@@ -21,5 +28,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.getRecipes();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
